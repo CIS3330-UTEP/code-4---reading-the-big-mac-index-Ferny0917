5248044a75b2a951e4be5a500f66baa7439b6ee6
Ferny0917 <lfvargas@miners.utep.edu>
2024-09-26 11:42:55 -0600

    First part of the test

--- code_4.py
+++ code_4.py
@@ -6,21 +6,13 @@ df = pd.read_csv('./big-mac-full-index.csv')
 
 
 
-year =  2010
-query_dt = f"((date >= ' {year}-01-01' and date <= '{year}-12-31'))"
-df_result = df.query(query_dt)
-
-country_code = 'arg'
-query_cc = f"(iso_a3 == '{country_code.upper()}')"
-sub_df = df_result.query(query_cc)
-
-
 def get_big_mac_price_by_year(year,country_code):
-    query_dt = f"((date >= ' {year}-01-01' and date <= '{year}-12-31'))"
+    query_dt = f"(date >= '{year}-01-01' and date <= '{year}-12-31')"
     df_result = df.query(query_dt)
     query_cc = f"(iso_a3 == '{country_code.upper()}')"
     sub_df = df_result.query(query_cc)
-    return sub_df
+    mean_price = sub_df['dollar_price'].mean()
+    return round (mean_price,2)
 
 def get_big_mac_price_by_country(country_code):
     pass # Remove this line and code your function
@@ -32,5 +24,5 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
 if __name__ == "__main__":
-    result_a = get_big_mac_price_by_year ()
-    print(result_a) 
+    result_a = get_big_mac_price_by_year(2012,'jpn')
+    print(result_a)
