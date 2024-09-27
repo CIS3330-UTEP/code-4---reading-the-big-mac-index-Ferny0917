d48c0d65050ef2a347a863124b2c9a7f66ae3371
Ferny0917 <lfvargas@miners.utep.edu>
2024-09-27 14:10:32 -0600

    part 2 of code 4

--- code_4.py
+++ code_4.py
@@ -7,15 +7,27 @@ df = pd.read_csv('./big-mac-full-index.csv')
 
 
 def get_big_mac_price_by_year(year,country_code):
+    # code to get the year simpler because it woudn't read it from the string
     query_dt = f"(date >= '{year}-01-01' and date <= '{year}-12-31')"
     df_result = df.query(query_dt)
+    #get the country code even if its lower case
     query_cc = f"(iso_a3 == '{country_code.upper()}')"
     sub_df = df_result.query(query_cc)
+    #get the dollar amount from the column and get the mean value
     mean_price = sub_df['dollar_price'].mean()
+    #answer in two values
     return round (mean_price,2)
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    #I just copy this line from the first code that focus on the country code
+    query_cc = f"(iso_a3 == '{country_code.upper()}')"
+    #query from country that we remove result cause we dont need date
+    sub_df = df.query(query_cc)
+    # mean of price 
+    mean_price = sub_df['dollar_price'].mean()
+    return round (mean_price,2)
+
+
 
 def get_the_cheapest_big_mac_price_by_year(year):
     pass # Remove this line and code your function
@@ -26,3 +38,5 @@ def get_the_most_expensive_big_mac_price_by_year(year):
 if __name__ == "__main__":
     result_a = get_big_mac_price_by_year(2012,'jpn')
     print(result_a)
+    result_b = get_big_mac_price_by_country('mex')
+    print(result_b)
